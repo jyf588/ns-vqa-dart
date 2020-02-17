8076a9730e4a80f746ec4b0f832c277b2b06efeb
Michelle Guo <shellguo95@gmail.com>
2020-02-17 12:24:40 -0800

    Create html page.

---
+++ html/README.md
@@ -0,0 +1,6 @@
+To start the server, run:
+
+```
+cd ns-vqa-dart/html
+python -m http.server 8000
+```
--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,26 +2,26 @@ cd scene_parse/attr_net
 
 time python run_test.py \
     --dataset dash \
-    --run_dir ~/outputs/ego_v004 \
-    --dataset_dir ~/datasets/ego_v004 \
-    --output_path ~/outputs/ego_v004/test.json \
-    --load_checkpoint_path ~/outputs/ego_v004/checkpoint_best.pt \
+    --run_dir ~/outputs/ego_v005 \
+    --dataset_dir ~/datasets/ego_v005 \
+    --output_path ~/outputs/ego_v005/test.json \
+    --load_checkpoint_path ~/outputs/ego_v005/checkpoint_best.pt \
     --split_id 20000 \
     --pred_attr \
     --pred_position \
     --pred_up_vector \
     --pred_height \
-    --coordinate_frame world
+    --coordinate_frame camera
 
 cd ../..
 
 time python bullet/metrics.py \
-    --dataset_dir ~/datasets/ego_v004 \
-	--pred_path ~/outputs/ego_v004/test.json \
-    --coordinate_frame world
+    --dataset_dir ~/datasets/ego_v005 \
+	--pred_path ~/outputs/ego_v005/test.json \
+    --coordinate_frame camera
 
 time python bullet/visualize.py \
-    --dataset_dir ~/datasets/ego_v004 \
-    --pred_path ~/outputs/ego_v004/test.json \
-    --output_dir ~/analysis/ego_v004 \
-    --coordinate_frame world
+    --dataset_dir ~/datasets/ego_v005 \
+    --pred_path ~/outputs/ego_v005/test.json \
+    --output_dir ~/analysis/ego_v005 \
+    --coordinate_frame camera
