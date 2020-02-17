45f46efc57d446f2c8adde1e4c6e4d0cfc456c05
Michelle Guo <shellguo95@gmail.com>
2020-02-17 12:47:49 -0800

    Image loading on html working.

---
+++ scripts/html.sh
@@ -0,0 +1,5 @@
+python bullet/html_images.py \
+    --dataset_dir ~/datasets/ego_v005 \
+	--pred_path ~/outputs/ego_v005/test.json \
+    --output_dir ~/analysis/ego_v005 \
+    --coordinate_frame camera
