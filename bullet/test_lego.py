2476a34bb2dbd644e0d2e27ce272e112d7817fb7
michguo <shellguo95@gmail.com>
2020-02-19 11:49:59 -0800

    Make lego (without studs) match the box height.

--- bullet/test_lego.py
+++ bullet/test_lego.py
@@ -10,8 +10,20 @@ def main():
 
     physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
     p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
-    oid = p.loadURDF("bullet/assets/lego_small.urdf")
-    oid = p.loadURDF("bullet/assets/box_small.urdf")
+    oid = p.loadURDF("bullet/assets/lego_small.urdf", basePosition=[0, 0, 0])
+    oid = p.loadURDF(
+        "bullet/assets/box_small.urdf", basePosition=[0.025, 0.15, 0]
+    )
+    oid = p.loadURDF("bullet/assets/lego.urdf", basePosition=[0, 0.25, 0])
+    oid = p.loadURDF("bullet/assets/box.urdf", basePosition=[0.04, 0.45, 0])
+    # lego = p.loadURDF(
+    #     "bullet/assets/lego.urdf", basePosition=[0, 0, 0], useFixedBase=True
+    # )
+    # box = p.loadURDF(
+    #     "bullet/assets/box.urdf",
+    #     basePosition=[0.04, 0.04, -0.1],
+    #     useFixedBase=True,
+    # )
     """
     x2.35
     7.2
@@ -24,11 +36,12 @@ def main():
         (-0.04, 0.04)
         (-0.09, 0.09)
     """
-    min_aabb, max_aabb = p.getAABB(oid)
-    print(min_aabb)
-    print(max_aabb)
+    # min_aabb, max_aabb = p.getAABB(lego)
+    # print(min_aabb)
+    # print(max_aabb)
     for _ in range(10000):
         time.sleep(1 / 240)
+        p.stepSimulation()
     p.disconnect()
 
 
