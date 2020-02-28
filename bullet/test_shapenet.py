a093daab66baaebba742f3234113ed91a4c8e187
michguo <shellguo95@gmail.com>
2020-02-28 14:45:29 -0800

    Get measurements for soda can.

--- bullet/test_shapenet.py
+++ bullet/test_shapenet.py
@@ -3,14 +3,19 @@ import pybullet as p
 import time
 
 
+CAN_HEIGHT_SCALE = 0.24
+CAN_RADIUS_SCALE = 0.11
+CAN_ORIGIN = [0.0, 0.0, 0.115]
+
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 shift = [0, 0, 0]
-meshScale = [1.0, 1.0, 1.0]
+meshScale = [CAN_RADIUS_SCALE * 2, CAN_HEIGHT_SCALE, CAN_RADIUS_SCALE * 2]
 
 visualShapeId = p.createVisualShape(
     shapeType=p.GEOM_MESH,
-    fileName="bullet/assets/model_normalized.obj",
+    fileName="bullet/assets/soda_can.obj",
     rgbaColor=[0.8, 0.0, 0.0, 1.0],
+    meshScale=meshScale,
     visualFrameOrientation=[
         math.cos(math.radians(90 / 2)),
         0,
@@ -20,15 +25,22 @@ visualShapeId = p.createVisualShape(
 )
 collisionShapeId = p.createCollisionShape(
     shapeType=p.GEOM_MESH,
-    fileName="bullet/assets/model_normalized.obj",
+    fileName="bullet/assets/soda_can.obj",
     # collisionFramePosition=shift,
-    # meshScale=meshScale,
+    meshScale=meshScale,
 )
 
 oid = p.createMultiBody(
     baseCollisionShapeIndex=collisionShapeId,
     baseVisualShapeIndex=visualShapeId,
-    basePosition=[0.0, 0.0, 0.5],
+    basePosition=CAN_ORIGIN,
+)
+
+oid = p.loadURDF(
+    "bullet/assets/cylinder.urdf",
+    # basePosition=[0.0, 0.0, -0.18],
+    basePosition=[0.0, 0.0, 0.0],
+    useFixedBase=True,
 )
 
 
