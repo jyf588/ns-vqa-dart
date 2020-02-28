a7351e8d45402f5caa583dae9e066443d11368d0
michguo <shellguo95@gmail.com>
2020-02-27 21:26:49 -0800

    Got shapenet sodacan loaded in pybullet.

---
+++ bullet/test_shapenet.py
@@ -0,0 +1,37 @@
+import pybullet as p
+import time
+
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+shift = [0, -0.02, 0]
+meshScale = [1.0, 1.0, 1.0]
+
+visualShapeId = p.createVisualShape(
+    shapeType=p.GEOM_MESH,
+    fileName="bullet/assets/model_normalized.obj",
+    rgbaColor=[1, 1, 1, 1],
+    specularColor=[0.4, 0.4, 0],
+    visualFramePosition=shift,
+    meshScale=meshScale,
+)
+collisionShapeId = p.createCollisionShape(
+    shapeType=p.GEOM_MESH,
+    fileName="bullet/assets/model_normalized.obj",
+    collisionFramePosition=shift,
+    meshScale=meshScale,
+)
+
+p.createMultiBody(
+    baseMass=1,
+    baseInertialFramePosition=[0, 0, 0],
+    baseCollisionShapeIndex=collisionShapeId,
+    baseVisualShapeIndex=visualShapeId,
+    basePosition=[0.0, 0.0, 0.0],
+    useMaximalCoordinates=True,
+)
+
+
+for _ in range(10000):
+    time.sleep(1 / 240)
+    p.stepSimulation()
+p.disconnect()
