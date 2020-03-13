ead5042046b7eb35a14f54c3732f1d23efb4cf01
Michelle Guo <shellguo95@gmail.com>
2020-03-13 15:46:37 -0700

    Add cylinder results.

---
+++ scripts/stacking_v002/eval_cyl.sh
@@ -0,0 +1,31 @@
+cd scene_parse/attr_net
+
+time python run_test.py \
+    --dataset dash \
+    --run_dir ~/outputs/stacking_v002_cyl \
+    --dataset_dir ~/datasets/stacking_v002_cyl \
+    --output_path ~/outputs/stacking_v002_cyl/pred.json \
+    --load_checkpoint_path ~/outputs/stacking_v002_cyl/checkpoint_best.pt \
+    --eval_start_id 20000 \
+    --eval_end_id 22000 \
+    --height 480 \
+    --width 480 \
+    --pred_attr \
+    --pred_size \
+    --pred_position \
+    --pred_up_vector \
+    --coordinate_frame camera \
+    --num_workers 8
+
+cd ../..
+
+python bullet/html_images.py \
+    --dataset_dir ~/datasets/stacking_v002_cyl \
+	--pred_path ~/outputs/stacking_v002_cyl/pred.json \
+    --html_dir ~/html/stacking_v002_cyl \
+    --coordinate_frame camera \
+    --n_objects 100
+
+python bullet/html.py \
+    --html_dir ~/html/stacking_v002_cyl \
+    --show_objects
