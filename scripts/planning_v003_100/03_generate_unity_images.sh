50199a219006e3e75f825a88d50810e053c641b4
michguo <shellguo95@gmail.com>
2020-04-18 10:17:23 -0700

    Update step 3 script.

--- scripts/planning_v003_100/03_generate_unity_images.sh
+++ scripts/planning_v003_100/03_generate_unity_images.sh
@@ -8,5 +8,6 @@ rm -rf ~/data/planning_v003_100/unity_output
 mkdir -p ~/data/planning_v003_100/unity_output
 time cp -r ~/workspace/lucas/unity/Captures/temp ~/data/planning_v003_100/unity_output/images
 time cp -r ~/data/temp_unity_data ~/data/planning_v003_100/unity_output/json
+cd ~/data
 time zip -r planning_v003_100.zip planning_v003_100
 time rsync -azP planning_v003_100.zip sydney:~/mguo/data/datasets/
