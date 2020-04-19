5913a79292fda0b7d1223ac3bc26b3f048c9cf5c
Michelle Guo <shellguo95@gmail.com>
2020-04-18 17:27:56 -0700

    update step 3

--- scripts/planning_v003_20K/03_generate_unity_images.sh
+++ scripts/planning_v003_20K/03_generate_unity_images.sh
@@ -1,15 +1,15 @@
 rm -rf ~/workspace/lucas/unity/Captures/temp
 time python demo/run_unity_from_states.py \
-    --states_dir ~/data/states/planning_v003_100 \
+    --states_dir ~/data/states/planning_v003_20K \
     --start_id 0 \
-    --end_id 100 \
+    --end_id 20000 \
     --camera_control center \
     --out_dir /Users/michelleguo/data/temp_unity_data
 
-rm -rf ~/data/planning_v003_100/unity_output
-mkdir -p ~/data/planning_v003_100/unity_output
-time cp -r ~/workspace/lucas/unity/Captures/temp ~/data/planning_v003_100/unity_output/images
-time cp -r ~/data/temp_unity_data ~/data/planning_v003_100/unity_output/json
+rm -rf ~/data/planning_v003_20K/unity_output
+mkdir -p ~/data/planning_v003_20K/unity_output
+time cp -r ~/workspace/lucas/unity/Captures/temp ~/data/planning_v003_20K/unity_output/images
+time cp -r ~/data/temp_unity_data ~/data/planning_v003_20K/unity_output/json
 cd ~/data
-time zip -r planning_v003_100.zip planning_v003_100
-time rsync -azP planning_v003_100.zip sydney:~/mguo/data/datasets/
+time zip -r planning_v003_20K.zip planning_v003_20K
+time rsync -azP planning_v003_20K.zip sydney:~/mguo/data/datasets/
