dd34985f524340e723fe55d14786a613e4741e85
Michelle Guo <shellguo95@gmail.com>
2020-04-21 16:26:11 -0700

    Fix path for step 2 script.

--- scripts/placing/v003_2K_20K/02_transfer_states.sh
+++ scripts/placing/v003_2K_20K/02_transfer_states.sh
@@ -1,6 +1,6 @@
 ROOT_DIR=/media/sdc3/mguo
 
 time rsync -azP sydney:$ROOT_DIR/data/states/full/placing_v003_2K_20K.zip ~/data/states/
-cd $ROOT_DIR/data/states
+cd ~/data/states
 rm -rf placing_v003_2K_20K
 time unzip placing_v003_2K_20K.zip
