c17e7b6216f7beaaccc41c09d3c011236f8305cb
Michelle Guo <shellguo95@gmail.com>
2020-04-21 20:08:03 -0700

    Update scripts.

--- scripts/placing/v003_2K_20K/04_generate_and_run.sh
+++ scripts/placing/v003_2K_20K/04_generate_and_run.sh
@@ -1,17 +1,17 @@
 ROOT_DIR=/media/sdc3/mguo
 
 cd $ROOT_DIR/data/datasets
-mkdir placing_v003_10_100
-mv placing_v003_10_100.zip placing_v003_10_100/
-cd placing_v003_10_100
-time unzip placing_v003_10_100.zip
+mkdir placing_v003_2K_20K
+mv placing_v003_2K_20K.zip placing_v003_2K_20K/
+cd placing_v003_2K_20K
+time unzip placing_v003_2K_20K.zip
 rm -rf unity_output
-cd placing_v003_10_100
+cd placing_v003_2K_20K
 mv unity_output ../
 cd ..
-rm -rf placing_v003_10_100
+rm -rf placing_v003_2K_20K
 cd ~/workspace/pytorch-rl-bullet
 
-time ./ns_vqa_dart/scripts/placing/v003_10_100/generate.sh
-time ./ns_vqa_dart/scripts/placing/v003_10_100/dry_run.sh
-time ./ns_vqa_dart/scripts/placing/v003_10_100/run.sh
+time ./ns_vqa_dart/scripts/placing/v003_2K_20K/generate.sh
+time ./ns_vqa_dart/scripts/placing/v003_2K_20K/dry_run.sh
+time ./ns_vqa_dart/scripts/placing/v003_2K_20K/run.sh
--- scripts/placing/v003_2K_20K/generate.sh
+++ scripts/placing/v003_2K_20K/generate.sh
@@ -1,6 +1,6 @@
-ROOT_DIR=~/mguo
-SRC_SET=placing_v003_10_100
-DST_SET=placing_v003_10_100
+ROOT_DIR=/media/sdc3/mguo
+SRC_SET=placing_v003_2K_20K
+DST_SET=placing_v003_2K_20K
 
 time python ns_vqa_dart/bullet/gen_dataset.py \
     --states_dir $ROOT_DIR/data/states/full/$SRC_SET \
@@ -10,4 +10,4 @@ time python ns_vqa_dart/bullet/gen_dataset.py \
     --camera_control stack \
     --coordinate_frame unity_camera \
     --start_sid 0 \
-    --end_sid 100
+    --end_sid 20000
